079255a19c5273dc80679ab0a9fe73d9972f8384
Cao Jiahang <caojiahang@formovie.cn>
2022-07-02 10:33:29 +0800

    fix: cli require。

--- supports/cli/bin/cli.js
+++ supports/cli/bin/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const pkg = require('@tarpit/cli/package.json')
-const { create_cli } = require('./')
+const { create_cli } = require('@tarpit/cli')
 
 create_cli(pkg).catch(err => {
     console.log(err)
