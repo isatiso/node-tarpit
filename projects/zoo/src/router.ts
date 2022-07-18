b5fb83f77fff87aa56e2889955b4736b383588dd
Plank <sieglive@gmail.com>
2022-07-18 11:10:09 +0800

    feat: 更新依赖规则。

--- projects/zoo/src/router.ts
+++ projects/zoo/src/router.ts
@@ -6,78 +6,20 @@
  * found in the LICENSE file at source root.
  */
 
-import { Inject, Optional, Platform, TpInspector, TpRoot, TpService } from '@tarpit/core'
+import { Platform, TpInspector, TpRoot } from '@tarpit/core'
 import { HttpServerModule, Post, RawBody, TpRouter } from '@tarpit/http'
-import { TestService1 } from './services/test-service.1'
-import { SymbolToken, TestService2 } from './services/test-service.2'
 
-@TpService()
-class TestService {
-    constructor(
-        @Optional()
-        @Inject('œœ-TpStartedAtaefaef')
-        private aaa: number,
-        ts: TestService1
-    ) {
-    }
-}
-
-@TpRouter('/', {
-    providers: [
-        // TestProducer,
-        TestService,
-        TestService1,
-        TestService2,
-
-        {
-            provide: SymbolToken,
-            useFactory: (aaa: number) => {
-                return 123
-            },
-            deps: [
-                [
-                    new Optional(),
-                    new Inject('œœ-TpStartedAt1111111')
-                ]
-            ]
-        }
-    ]
-})
+@TpRouter('/')
 class TestRouter {
 
-    constructor(
-        private inspector: TpInspector,
-        @Optional()
-        @Inject('œœ-TpStartedAtaefaef')
-        private aaa: number,
-        // private producer: TestProducer,
-        // private platform: Platform
-    ) {
+    constructor() {
     }
 
     @Post('asd')
     async test(
-        ts: TestService,
         buf: RawBody,
-        @Optional()
-        @Inject('œœ-TpStartedAtaefaef')
-            aaa: number,
     ) {
-        await new Promise(resolve => {
-            setTimeout(() => resolve(null), 2000)
-        })
-        return {}
-
-        // const detect_result = await detector.detect<{ name: string, email: string, count: number }>()
-        // if (detect_result.type === 'json') {
-        //     const body = detect_result.body
-        //     const name = body.ensure('name', Jtl.string)
-        //     const email = body.ensure('email', Jtl.string)
-        //     const count = body.ensure('count', Jtl.multiple_of(7))
-        //     return { name, email, count }
-        // } else {
-        //     return null
-        // }
+        return buf
     }
 }
 
@@ -106,10 +48,6 @@ export class TestRoot {
 
     platform.bootstrap(TestRoot).start()
     await platform.expose(TpInspector)?.wait_start()
-    // const producer = platform.expose(TestProducer)!
-    // console.log(producer)
-
-    // setInterval(() => producer.send_topic_message.send({ a: 'asd', b: Date.now() }), 500)
 })()
 
 
