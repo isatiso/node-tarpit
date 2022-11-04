890ce5e8fb979b5f3d84a6ff2a0b2ba693c5b9d7
Plank <sieglive@gmail.com>
2022-11-04 09:33:02 +0800

    record node

--- modules/http/src/tools/handler-book.ts
+++ modules/http/src/tools/handler-book.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at source root.
  */
 
-import { MatchFunction } from 'path-to-regexp'
+import { parse, match, MatchFunction } from 'path-to-regexp'
 import { ApiMethod, HttpHandler, HttpHandlerDescriptor } from '../__types__'
 
 interface HttpHandlerMap {
@@ -18,31 +18,80 @@ interface HttpHandlerMap {
 }
 
 interface RegExpNode {
-    regex: MatchFunction
-    method: string
-    handler: HttpHandler
+    match: MatchFunction
+    handler: HttpHandlerMap
 }
 
-interface SearchNode {
-    children: { [segment: string]: SearchNode }
-    regex: RegExpNode[]
-    method: ApiMethod[]
-    handler: HttpHandler
+interface PathNode {
+    children: { [segment: string]: PathNode }
+    matchers: RegExpNode[]
+    handler?: HttpHandlerMap
 }
 
 export class HandlerBook {
 
     private book = new Map<string, HttpHandlerMap>()
 
-    record(method: ApiMethod, path: string, handler: HttpHandler) {
-        const regular_method = method.toUpperCase() as ApiMethod
-        if (!this.book.has(path)) {
-            this.book.set(path, { _allows: ['OPTIONS'] })
+    private index: { [path: string]: { handler: HttpHandlerMap } } = {}
+    private root: PathNode = { children: {}, matchers: [] }
+
+    init_path_node(segments: string[]) {
+        let node = this.root
+        for (const segment of segments) {
+            if (!node.children[segment]) {
+                node.children[segment] = { children: {}, matchers: [] }
+            }
+            node = node.children[segment]
         }
-        const map = this.book.get(path)!
-        map[regular_method] = handler
-        if (!map._allows.includes(method)) {
-            method === 'GET' ? map._allows.push('HEAD', 'GET') : map._allows.push(method)
+        return node
+    }
+
+    update_handler_node(node: HttpHandlerMap, method: ApiMethod, handler: HttpHandler) {
+        node[method] = handler
+        if (!node._allows.includes(method)) {
+            method === 'GET' ? node._allows.push('HEAD', 'GET') : node._allows.push(method)
+        }
+    }
+
+    record(method: ApiMethod, path: string, handler: HttpHandler) {
+        method = method.toUpperCase() as any
+        if (this.index[path]) {
+            this.update_handler_node(this.index[path].handler, method, handler)
+        } else {
+            const handler_node: HttpHandlerMap = { _allows: ['OPTIONS'] }
+            this.update_handler_node(handler_node, method, handler)
+
+            const tokens = parse(path)
+            if (typeof tokens[0] === 'string') {
+                const static_path = tokens[0].replace(/^\//g, '').split('/')
+                const node = this.init_path_node(static_path)
+                if (tokens[1]) {
+                    const regex_node = { match: match(path), handler: handler_node }
+                    node.matchers.push(regex_node)
+                    this.index[path] = regex_node
+                } else {
+                    node.handler = handler_node
+                    this.index[path] = node as { handler: HttpHandlerMap }
+                }
+            }
+        }
+
+        // const regular_method = method.toUpperCase() as ApiMethod
+        // if (!this.book.has(path)) {
+        //     this.book.set(path, { _allows: ['OPTIONS'] })
+        // }
+        // const map = this.book.get(path)!
+        // map[regular_method] = handler
+        // if (!map._allows.includes(method)) {
+        //     method === 'GET' ? map._allows.push('HEAD', 'GET') : map._allows.push(method)
+        // }
+    }
+
+    get_node(path: string) {
+        const segments = path.split('/')
+        let node = this.root
+        for (const segment of segments) {
+
         }
     }
 
