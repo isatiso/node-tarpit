726655545f823096f354c1f980468907ab4846bb
Plank Cao <sieglive@gmail.com>
2025-04-07 14:55:32 +0800

    temp2

--- modules/worker-threads/src/builtin/tp-thread.spec.ts
+++ modules/worker-threads/src/builtin/tp-thread.spec.ts
@@ -38,6 +38,7 @@ describe('tp-thread.ts', function() {
         })
 
         it('should execute task', async function() {
+            this.timeout(10000);
             const cases: number[][] = []
             for (let i = 0; i < 100; i++) {
                 const a = Math.floor(Math.random() * 1000)
@@ -52,6 +53,7 @@ describe('tp-thread.ts', function() {
         })
 
         it('should execute async task', async function() {
+            this.timeout(10000)
             const cases: number[][] = []
             for (let i = 0; i < 100; i++) {
                 const a = Math.floor(Math.random() * 1000)
